f642ce41410f8ed8c062f202346b7b0609764cd4
Adrien Hallet <adrienhallet01@gmail.com>
2022-03-25 20:26:05 +0100

    feat(dsl): Allow trailing comma

--- src/dsl.rs
+++ src/dsl.rs
@@ -1,15 +1,13 @@
-#![feature(trace_macros)]
-
 #[macro_export]
 macro_rules! define {
-    ($in: literal -$transition: literal -> $out: literal) => {
+    ($in: literal -$transition: literal -> $out: literal $(,)?) => {
         {
             let mut _var = $crate::machine::Machine::new();
             define!(_var => $in - $transition -> $out)
         }
     };
 
-    ($in: literal -$transition: literal -> $out: literal $(, $in2: literal -$transition2: literal -> $out2: literal)*) => {{
+    ($in: literal -$transition: literal -> $out: literal $(, $in2: literal -$transition2: literal -> $out2: literal)* $(,)?) => {{
         {
             let mut _var = $crate::model::machine::Machine::new();
             define!(
@@ -18,14 +16,14 @@ macro_rules! define {
         }
     }};
 
-    ($machine: ident => $(,)* $in: literal -$transition: literal -> $out: literal $(, $in2: literal -$transition2: literal -> $out2: literal)+) => {{
+    ($machine: ident => $(,)? $in: literal -$transition: literal -> $out: literal $(, $in2: literal -$transition2: literal -> $out2: literal)+) => {{
         {
             $machine.transitions.push($transition);
             define!($machine => $(, $in2 -$transition2 -> $out2)?)
         }
     }};
 
-    ($machine: ident => $(,)* $in: literal -$transition: literal -> $out: literal) => {
+    ($machine: ident => $(,)? $in: literal -$transition: literal -> $out: literal) => {
         {
             $machine.transitions.push($transition);
             $machine
