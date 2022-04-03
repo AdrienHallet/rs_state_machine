18df05de6a4f738c4b1fb6754f32ff24d7d61a8c
Adrien Hallet <adrienhallet01@gmail.com>
2022-04-03 15:05:09 +0200

    chore: Test Transitionable

--- src/tests/transitionable_test.rs
+++ src/tests/transitionable_test.rs
@@ -13,3 +13,21 @@ impl Transitionable for DummyTransitionable {
         self.state = new_state;   
     }
 }
+
+#[test]
+fn should_get_state() {
+    let state = "INPUT";
+    let transitionable = DummyTransitionable{ state: state.to_string() };
+    assert_eq!(state.to_string(), transitionable.get_state());
+}
+
+#[test]
+fn should_set_state() {
+    let state_before = "INPUT";
+    let state_after  = "OUTPUT";
+    let mut transitionable = DummyTransitionable{ state: state_before.to_string() };
+
+    assert_eq!(state_before.to_string(), transitionable.get_state());
+    transitionable.set_state(state_after.to_string());
+    assert_eq!(state_after.to_string(), transitionable.get_state());
+}
